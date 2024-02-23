a58ca2b55d88844c8113d12c99867ec60d770dea
boraayhan <boraayhanwebsite@gmail.com>
2024-02-22 17:42:35 -0700

    added cool mobile support

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+
+var l = ["A web developer", "A test code writer", "An idiot"];
+var el = document.getElementById("app");
+
+for (var s of l) {
+    var i = 0;
+    while(i < s.length) {
+        el.innerHTML += s.charAt(i);
+        i++;
+    }
+}
