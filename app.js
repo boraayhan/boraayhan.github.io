d8aedc416d3e2a19616fec938520d3e941982975
boraayhan <boraayhanwebsite@gmail.com>
2024-02-23 17:29:34 -0700

    added cooler mobile support

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
+    var el = document.getElementById("mobile");
+    if (el.style.display === "none") {
+        el.style.display = "block";
+    } 
+    else {
+        el.style.display = "none";
+    } 
 }
