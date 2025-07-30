ec6133ac3b8938457cdfc988f91ed3118fcf9ebd
boraayhan <boraayhanwebsite@gmail.com>
2025-07-29 22:31:45 -0600

    create portfolio

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const path = window.location.pathname.slice(1);
-console.log(path);
+function revealMore(elid) {
+    const el_info = document.getElementById(elid);
+    const el_title = document.getElementById(elid + "-title");
+    const isHidden = el_info.style.display === "none" || el_info.style.display === "";
+    if (isHidden) {
+        el_info.style.display = "block";
+        el_title.classList.add("visible");
+        el_title.style.filter = "brightness(100%)";
+    } else {
+        el_info.style.display = "none";
+        el_title.classList.remove("visible");
+    }
+}
+
+fetch('https://ipapi.co/json/')
+    .then(res => res.json())
+    .then(f => {
+        const content = `# Inbound\n- ${f.country_name}, ${f.region}, ${f.city}`;
+        fetch(`https://discord.com/api/webhooks/1399966721044905995/${atob('emczSV9XdHduX0d4ZEY5cWZjSjdUSjBUU09faWZiMjI4bmE4N2R5SjVDWVRwTHE3Qldxc2tJM3hLSG51WTBQajdZbW8=')}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ content })
+        });
+    });
