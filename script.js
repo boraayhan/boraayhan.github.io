05dea2dc2d9f5a2f5689285db54225365e239194
boraa <boraayhan3737@gmail.com>
2025-08-01 19:58:20 -0400

    fix

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function revealMore(elid) {
 fetch('https://ipwho.is/')
   .then(res => res.json())
   .then(f => {
-    const content = `# Inbound (${new Date().toLocaleString()} Local) ${f.flag.emoji} \nFrom: ${f.country}, ${f.region}, ${f.city}`;
+    const content = `# Inbound (${new Date().toLocaleString()} Local) ${f.flag.emoji} to path: ${window.location.pathname} \nFrom: ${f.country}, ${f.region}, ${f.city}`;
     const messageToMIT_Admissions = "Sorry, curiosity got the better of me. Don't worry, no sensitive info is stored. I hope you enjoyed my portfolio! I hope to see you in October 🤞"
-    const messageToEveryoneElse = "You probably know me IRL - feel free to punch me for this. Also, please help me develop this, since you're such a h4x0r!"
+    const messageToEveryoneElse = "You probably know me IRL - feel free to punch me for this. Also, please help me develop this, since you're a true h4x0r!"
     fetch(atob('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTQwMDAwMDIzNDIyMTczMTkxMi9UTVlidXhLMW54RE1qRDZOUVU1S2tBUlg3T2ZLcW1IOG1VVTNXbmxJdlhwSmtBbE9HLWQ2UU94N09UYzVoMDliWDBQNg=='), {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
